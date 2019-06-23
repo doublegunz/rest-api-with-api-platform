1e1bd2dfa36a2f511d5554d7054202ae9dc6b34a
Gun Gun Priatna <gungunpriatna02@gmail.com>
2019-06-23 09:07:40 +0700

    init repo

---
+++ api/src/DataFixtures/AccountFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Account;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AccountFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i=1; $i <= 20 ; ++$i) {
+            $account = new Account();
+            $account->setUsername('Android no. ' .$i);
+            $account->setIsActive(0 == $i % 2 ? false : true);
+            $manager->persist($account);
+        }
+
+        $manager->flush();
+    }
+}
